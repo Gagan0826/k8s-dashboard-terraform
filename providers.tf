379928ef91747974a8f8c415210dbfa25e016dbd
Gagan <gaganbrgirish26@gmail.com>
2025-05-28 16:26:09 +0530

    added default context

--- providers.tf
+++ providers.tf
@@ -12,10 +12,12 @@ terraform {
 }
 provider "kubernetes" {
   config_path = "~/.kube/config"
+  config_context = "minikube"
 }
 
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
+    config_context = "minikube"
   }
 }
